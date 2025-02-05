1252a25101508280683eec7305ffab5bcbc89b30
rorou4i <rosie_bortoluzzi@yahoo.co.uk>
2025-02-05 11:21:43 +0000

    transferred Forecast html from javascript

--- src/index.js
+++ src/index.js
@@ -53,7 +53,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="weather-forecast-day">
+  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-icon">⛅</div>
+  <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temp"><strong>15&deg;</strong></div>
+    <div class="weather-forecast-temp">9&deg;</div>
+  </div>
+</div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("London");
+
+displayForecast();
