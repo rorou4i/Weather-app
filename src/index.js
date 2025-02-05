65eacdc31bc9d67759b829ee42efb0ea1b00e25b
rorou4i <rosie_bortoluzzi@yahoo.co.uk>
2025-02-05 13:05:24 +0000

    added format day and display forecast functions in JS

--- src/index.js
+++ src/index.js
@@ -55,6 +55,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "4d9aaed80tfoc14d5c34f5ae333bcd5b";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -62,22 +69,28 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
 <div class="weather-forecast-day">
-  <div class="weather-forecast-date">${day}</div>
-  <div class="weather-forecast-icon">⛅</div>
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
+  <div>
+  <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+  </div>
   <div class="weather-forecast-temperatures">
-    <div class="weather-forecast-temp"><strong>15&deg;</strong></div>
-    <div class="weather-forecast-temp">9&deg;</div>
+    <div class="weather-forecast-temp"><strong>${Math.round(
+      day.temperature.maximum
+    )}&deg;</strong></div>
+    <div class="weather-forecast-temp">${Math.round(
+      day.temperature.minimum
+    )}&deg;</div>
   </div>
 </div>`;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
